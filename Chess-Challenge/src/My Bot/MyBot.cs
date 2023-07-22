86e5f3777a98a60c9f96af5d5ea4ab96c2c29eee
Andy Coates <andyhcoates@gmail.com>
2023-07-22 07:54:02 +0100

    strange things

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,36 @@
 ﻿using ChessChallenge.API;
 
+const int maxLevel = 0;
+
 public class MyBot : IChessBot
 {
     public Move Think(Board board, Timer timer)
     {
         Move[] moves = board.GetLegalMoves();
         return moves[0];
+        //return GetBestMove(board, timer);
+    }
+
+    private Move GetBestMove(Board board, Timer timer, int level = 0)
+    {
+        Move[] moves = board.GetLegalMoves();
+        var boardString = board.GetFenString();
+
+        foreach (var move in moves)
+        {
+            var testBoard = Board.CreateBoardFromFEN(boardString);
+
+            testBoard.MakeMove(move);
+            var score = ScoreBoard(testBoard);
+        }
+        return moves[0];
+    }
+
+    private float ScoreBoard(Board board)
+    {
+        var score = 0.0f;
+
+
+        return score;
     }
 }
